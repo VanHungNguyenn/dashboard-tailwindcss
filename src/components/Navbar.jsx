ade17a0a18aa6e0031496480e562bf1336da50b7
VanHung <vanhungnguyenbku@gmail.com>
2023-04-25 23:03:38 +0700

    complete navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,16 +19,41 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => {
 		>
 			<span
 				style={{ backgroundColor: dotColor }}
-				// className='absolute inline-flex rounded-full h-2 w-2 top-2 right-2'
-			>
-				{icon}
-			</span>
+				className='absolute inline-flex rounded-full h-2 w-2 top-2 right-2'
+			></span>
+			{icon}
 		</button>
 	)
 }
 
 const Navbar = () => {
-	const { activeMenu, setActiveMenu } = useStateContext()
+	const {
+		activeMenu,
+		setActiveMenu,
+		isClicked,
+		setIsClicked,
+		handleClick,
+		screenSize,
+		setScreenSize,
+	} = useStateContext()
+
+	useEffect(() => {
+		const handleResize = () => setScreenSize(window.innerWidth)
+
+		window.addEventListener('resize', handleResize)
+
+		handleResize()
+
+		return () => window.removeEventListener('resize', handleResize)
+	}, [])
+
+	useEffect(() => {
+		if (screenSize < 900) {
+			setActiveMenu(false)
+		} else {
+			setActiveMenu(true)
+		}
+	}, [screenSize])
 
 	return (
 		<div className='flex items-center justify-between p-2 md:mx-6 relative'>
@@ -39,6 +64,50 @@ const Navbar = () => {
 				color='blue'
 				dotColor=''
 			/>
+			<div className='flex items-center'>
+				<NavButton
+					title='Cart'
+					customFunc={() => handleClick('cart')}
+					icon={<FiShoppingCart />}
+					color='blue'
+					dotColor=''
+				/>
+				<NavButton
+					title='Chat'
+					customFunc={() => handleClick('chat')}
+					icon={<BsChatLeft />}
+					color='blue'
+					dotColor='#03c9d7'
+				/>
+				<NavButton
+					title='Notification'
+					customFunc={() => handleClick('notification')}
+					icon={<RiNotification3Line />}
+					color='blue'
+					dotColor='#03c9d7'
+				/>
+				<div
+					className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg'
+					onClick={() => handleClick('userProfile')}
+				>
+					<img
+						className='rounded-full w-8 h-8'
+						src={avatar}
+						alt='avatar'
+					/>
+					<p>
+						<span className='text-gray-400 text-14'>Hi, </span>
+						<span className='text-gray-400 text-14 font-bold ml-1'>
+							Michael
+						</span>
+					</p>
+					<MdKeyboardArrowDown className='text-gray-400 text-14' />
+				</div>
+				{isClicked.cart && <Cart />}
+				{isClicked.chat && <Chat />}
+				{isClicked.notification && <Notification />}
+				{isClicked.userProfile && <UserProfile />}
+			</div>
 		</div>
 	)
 }
